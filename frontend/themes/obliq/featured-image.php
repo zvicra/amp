be080f6d95bd9e3f97953131a816c602ed590ffc
zvicra <165465562+zvicra@users.noreply.github.com>
2024-04-11 19:58:58 +0200

    Delete frontend directory

--- frontend/themes/obliq/featured-image.php
+++
@@ -1,9 +0,0 @@
-<?php
-$featured_image =  has_post_thumbnail();
-
-if( !$featured_image ) {
-	return;
-}
-
-?>
-<amp-img layout="responsive" class="vw vh" width="375" height="667" src="<?php the_post_thumbnail_url( 'full' ); ?>"></amp-img>
